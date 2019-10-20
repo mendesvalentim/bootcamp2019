5bbba1b32e8eaf2e3ed7b13c5cfd31e9d62f460e
Bruno M. Valentim <mendesvalentim@gmail.com>
2019-10-20 14:36:33 -0400

    implementando sequelize

--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -1,9 +1,9 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({message: 'O Pedro e Nicolly!!!'})
+  return res.json({ message: 'O Pedro e Nicolly!!!' });
 });
 
 export default routes;
