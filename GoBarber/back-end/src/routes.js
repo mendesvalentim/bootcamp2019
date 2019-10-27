273acad29ac7b3141f17d6693aa40a4c4e8d14b9
Bruno M. Valentim <mendesvalentim@gmail.com>
2019-10-27 14:55:18 -0400

    adicionei formação do hash da senha

--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/Controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Bruno M. Valentim',
-    email: 'mendesvalentim@gmail',
-    password_hash: '12345678',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
