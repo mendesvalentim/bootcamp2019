5e1c029b49ee2aedd6066287da9a7d086459dbc0
Bruno M. Valentim <mendesvalentim@gmail.com>
2019-10-28 19:18:21 -0400

    adicionei jwt e validaçao com token

---
+++ GoBarber/back-end/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b012cfa1784890ce679a47e3748279f2',
+  expiresIn: '30d',
+};
--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/Controllers/UserController';
+import SessionController from './app/Controllers/SessionControler';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
